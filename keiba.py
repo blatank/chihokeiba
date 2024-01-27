680f1edbfe0563ebfb740e2a3683267da6020615
blatank <paddocknote@gmail.com>
2024-01-27 10:39:16 +0900

    2024/01/27開勉強会開始前

---
+++ keiba.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+
+import requests
+import re
+url = "https://www.keiba.go.jp/KeibaWeb/TodayRaceInfo/DebaTable?k_raceDate=2024%2f01%2f27&k_raceNo=10&k_babaCode=32"
+res = requests.get(url)
+soup = BeautifulSoup(res.text, "html.parser")
+
+names = soup.find_all("a", class_="horseName")
+races = soup.find_all("div", class_="raceInfo")
+card = soup.find_all("section", class_="cardTable")
+# races = soup.find_all("div", class_="cardTable raceInfo")
+tokei = re.findall(r'\d:\d\d\.\d　', str(card))
+
+bamei = []
+keibajou = []
+kyori = []
+baba = []
+time = []
+
+for horse in names:
+  # print(horse.get_text())
+  bamei.append(horse.get_text())
+  time.append([])
+  keibajou.append([])
+  kyori.append([])
+  baba.append([])
+
+a = 0
+for t in tokei:
+  time[int(a / 5)].append(t.replace('　', ''))
+  a = a + 1
+
+# b = 0
+# for race in races:
+#   # t = race.get_text()
+#   r = re.split(r'<br\/*>',str(race))
+#   racename = re.split(r'　',r[1])
+#   ba = re.split(r'　',r[0]) 
+#   if len(racename) > 1:
+#     keibajou[int(b / 5)].append(racename[0])
+#     kyori[int(b / 5)].append(racename[1])
+#     baba[int(b / 5)].append(ba[1])
+#   b = b + 1
+
+print(bamei)
+# print(keibajou)
+# print(kyori)
+# print(baba)
+print(time)
+# print(len(races))
