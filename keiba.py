a1baaca37973342c8cdeb53cac2d865505352c7c
blatank <paddocknote@gmail.com>
2024-03-21 00:00:06 +0900

    クラス化

--- keiba.py
+++ keiba.py
@@ -9,6 +9,7 @@ class Keiba:
     self.__url = url
     self.__courses = []
     self.__distances = []
+    self.__horses = []
 
     self.analyze()
   
@@ -32,32 +33,42 @@ class Keiba:
     self.__card = soup.find_all("section", class_="cardTable")
     self.__tokei = soup.select("tbody > tr")#名前変更予定
 
-    self.__bamei = []
-    self.__keibajou = []
-    self.__kyori = []
-    self.__baba = []
-    self.__time = []
+    names = []
+    time = []
+    # self.__keibajou = []
+    # self.__kyori = []
+    # self.__baba = []
+    # self.__time = []
 
     #馬名取り出し処理
+    no = 1
     for horse in self.__names:
-      self.__bamei.append(horse.get_text())#OK
-      self.__time.append([])#OK
-      self.__keibajou.append([])#OK
-      self.__kyori.append([])#OK
-      self.__baba.append([])
+      self.__horses.append(Horse(horse.get_text(), no))
+      no += 1
+      time.append([])
+      # self.__time.append([])#OK
+      # self.__keibajou.append([])#OK
+      # self.__kyori.append([])#OK
+      # self.__baba.append([])
 
     #走破時計取り出し処理
-    for i in range(len(self.__names)):
+    for i in range(len(self.__horses)):
+      # TODO:何を目的としているかコメントに残す
       t2 = re.split(r'</td>',str(self.__tokei[i * 5 + 5]))
+
+      # 馬柱に載っているのは過去5走
+      # そのデータを分解し、Horseにセットする
       for j in range(5):
         tokei2 = re.findall(r'\d:\d\d\.\d', t2[2+j])
+
+        # データが空(出走数が少ない場合など)ではない？
         if len(tokei2) > 0:
-          self.__time[i].append(tokei2[0])
+          time[i].append(tokei2[0])
         else:
-          self.__time[i].append("")
+          time[i].append("")
 
     #競馬場取り出し処理
-    for i in range(len(self.__names)):
+    for i in range(len(self.__horses)):
       for j in range(5):
         k2 = re.split(r'<br/>', str(self.__races[i * 5 +j]))
         if len(k2) > 1:
@@ -73,8 +84,9 @@ class Keiba:
             place = ""
             dis = ""
 
-        self.__keibajou[i].append(place)
-        self.__kyori[i].append(dis)
+        # self.__keibajou[i].append(place)
+        # self.__kyori[i].append(dis)
+        self.__horses[i].addHistory(place, dis, time[i][j])
 
   # 時計順で出力
   def outputHourseTime(self):
@@ -91,28 +103,63 @@ class Keiba:
     top_time = []
 
     # 該当データ検索
-    for i in range(len(self.__names)):
-      moti = ""
-
-      # 過去5走から検索する
-      for j in range(5):
-        if self.__keibajou[i][j] == course and self.__kyori[i][j] == distance:
-          if moti == "" or moti > self.__time[i][j]:
-            # 除外・取消を除く
-            if self.__time[i][j] != "":
-              # 持ち時計-馬番の形で一時保存
-              moti = self.__time[i][j] + "-" + str(i+1)
-      
-      # その馬の最速タイムとして登録
-      top_time.append(moti)
+    for horse in self.__horses:
+      time = horse.getTopTime(course, distance)
+      if time != "":
+        top_time.append(time + "-" + str(horse.getNo()))
     
     # ソートして出力する
     top_time.sort()
-    for s in top_time:
-      if s != "":
+    for time_str in top_time:
+      if time_str != "":
         # 持ちタイムと馬番を分離して、馬番を前に出して出力
-        s2 = re.split(r'-', s)
-        print(s2[1] + "番" + self.__bamei[int(s2[1]) - 1] +"：" + s2[0])
+        # splited_str[0]：タイム
+        # splited_str[1]：馬番
+        splited_str = re.split(r'-', time_str)
+        h = self.__horses[int(splited_str[1]) - 1]
+        print(splited_str[1] + "番" + h.getName() +"：" + splited_str[0])
+
+class Horse:
+  def __init__(self, name, no):
+    self.__name = name
+    self.__no = no
+    self.__histories = []
+
+  def getName(self):
+    return self.__name
+  
+  def getNo(self):
+    return self.__no
+
+  def addHistory(self, course, distance, time):
+    self.__histories.append(History(course, distance, time))
+
+  def getTopTime(self, course, distance):
+    time = ""
+    for history in self.__histories:
+      if (history.hasHistory(course, distance)):
+        t = history.getTime()
+
+        if time == "" or time > t:
+          time = t
+
+    return time
+
+class History:
+  def __init__(self, course, distance, time):
+    self.__course = course
+    self.__distance = distance
+    self.__time = time
+
+  def getTime(self):
+    return self.__time
+  
+  def hasHistory(self, course, distance):
+    if (self.__course == course and self.__distance == distance):
+      return True
+    else:
+      return False
+  
 
 # test(あとで別ファイルにする)
 race = Keiba(sys.argv[1])
