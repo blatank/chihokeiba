0553059e93a30e6529ffb00a240656b0c5425902
blatank <paddocknote@gmail.com>
2024-03-26 23:39:44 +0900

    RaceCourseクラスを追加。近い距離を問合せするテストを追加

---
+++ racecourse.py
@@ -0,0 +1,22 @@
+class RaceCourse:
+  def __init__(self, course, distance):
+    self.__course = course
+    self.__distance = distance
+
+  def getCourse(self):
+    return self.__course
+  
+  def getDistance(self):
+    return self.__distance
+  
+  # 設定された競馬場・距離に近い競馬場・距離を返す
+  def esitimateCourse(self):
+    eCourses = []
+
+    if self.__course == "佐賀":
+      if self.__distance == "右1400":
+        eCourses.append(RaceCourse("佐賀", "右1300"))
+      elif self.__distance == "右1300":
+        eCourses.append(RaceCourse("佐賀", "右1400"))
+
+    return eCourses
--- test_race.py
+++ test_race.py
@@ -1,6 +1,7 @@
 import unittest
 from race import Race
 from history import History
+from racecourse import RaceCourse
 
 test_url = "https://www.keiba.go.jp/KeibaWeb/TodayRaceInfo/DebaTable?k_raceDate=2024%2f03%2f13&k_raceNo=12&k_babaCode=24"
 invalid_url = "https://blatan.info/"
@@ -24,3 +25,27 @@ class TestRace(unittest.TestCase):
     self.assertTrue(history.hasHistory("佐賀", "右1400"))
     self.assertFalse(history.hasHistory("高知", "右1400"))
     self.assertFalse(history.hasHistory("佐賀", "右1300"))
+
+  def test_estimate_distance(self):
+    # 佐賀1400mのレースを設定
+    saga = RaceCourse("佐賀", "右1400")
+
+    # 近い距離のデータを取得
+    eCourse = saga.esitimateCourse()
+
+    # 近い距離は1つ
+    self.assertEqual(1, len(eCourse))
+
+    # その1つは佐賀・右1300m
+    self.assertEqual("佐賀", eCourse[0].getCourse())
+    self.assertEqual("右1300", eCourse[0].getDistance())
+
+    # あり合えないデータを設定
+    saga = RaceCourse("佐賀", "右3200")
+
+    # 近い距離のデータを取得
+    eCourse = saga.esitimateCourse()
+
+    # 近い距離のデータ無し
+    self.assertEqual(0, len(eCourse))
+    
