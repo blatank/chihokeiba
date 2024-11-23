3fdd55fa8995e9b939e4d18b71c514df43292cd4
blatank <paddocknote@gmail.com>
2024-11-23 21:13:06 +0900

    解析期間設定の変更後もテスト通るように修正

--- test_history.py
+++ test_history.py
@@ -1,10 +1,13 @@
 import unittest
 from history import History
 from racecourse import RaceCourse
+import datetime
 
 saga1300 = RaceCourse("佐賀", "右1300")
 saga1400 = RaceCourse("佐賀", "右1400")
 kouchi1400 = RaceCourse("高知", "右1400")
+testdate = datetime.datetime(2024, 4, 16)
+testdata_invalid = datetime.datetime(2023, 4, 16)
 
 class TestHistory(unittest.TestCase):
   def test_history(self):
@@ -21,12 +24,13 @@ class TestHistory(unittest.TestCase):
     self.assertNotEqual("1:30:1", history.getTimeStr())
     
     # getDateのテスト
-    self.assertEqual("", history.getDate())
-    self.assertNotEqual("24.04.16", history.getDate())
+    # 空白時は1999/1/1扱い
+    self.assertEqual(datetime.datetime(1999, 1, 1), history.getDate())
+    self.assertNotEqual(datetime.datetime(2024, 4, 16), history.getDate())
     
     # 適当に戦績を追加(日付付き)
     history = History(saga1400, "1:30:0", "24.04.16")
 
     # getDateのテスト
-    self.assertEqual("24.04.16", history.getDate())
-    self.assertNotEqual("23.04.16", history.getDate())
+    self.assertEqual(testdate, history.getDate())
+    self.assertNotEqual(testdata_invalid, history.getDate())
